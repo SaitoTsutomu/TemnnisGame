85188208dd1e02127522c3e6a11f8be44f248be4
Saito Tsutomu <tsutomu@kke.co.jp>
2012-08-22 04:36:28 +0900

    Create

---
+++ TennisGame/Server.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace TennisGame
+{
+	public class Server : MarshalByRefObject
+	{
+		public const int WinPoint = 3;
+		public const double Size = 0.08;
+		private double[] barPos = new double[2];
+		public double BarPos(bool bFirst) { return barPos[bFirst ? 0 : 1]; }
+		private int[] score = new int[2];
+		public int Score(bool bFirst) { return score[bFirst ? 0 : 1]; }
+		private Point discPos;
+		public Point DiscPos { get { return discPos; } }
+		private Vector dir;
+
+		private int count;
+		public bool IsWaiting;
+		public bool IsRunning;
+		public Server()
+		{
+			Reset();
+			count = 24;
+			IsWaiting = true;
+			IsRunning = !false;
+		}
+		public void Reset()
+		{
+			barPos[0] = 0;
+			barPos[1] = 1 - Window1.barRate;
+			score[0] = score[1] = 0;
+			Init(true);
+			IsRunning = true;
+		}
+		private void Init(bool bFirst)
+		{
+			discPos = new Point(0.5, 0.5);
+			dir = new Vector((new Random().NextDouble() * 0.05 - 0.025),
+				0.05 * (bFirst ? -1 : 1));
+			count = 8;
+		}
+		public void Do()
+		{
+			if (IsWaiting) return;
+			if (count > 0)
+			{
+				--count;
+				return;
+			}
+			discPos += dir;
+			if (discPos.X < Size)
+			{
+				discPos.X = -discPos.X + 2 * Size;
+				dir.X = -dir.X;
+			}
+			else if (discPos.X > 1 - Size)
+			{
+				discPos.X = -discPos.X + 2 * (1 - Size);
+				dir.X = -dir.X;
+			}
+			for (int i = 0; i < 2; i++)
+			{
+				if (i == 0 && discPos.Y >= Size / 2) continue;
+				else if (i == 1 && discPos.Y <= 1 - Size / 2) continue;
+				double x = discPos.X - barPos[i];
+				if (x < 0 || x > Window1.barRate)
+				{
+					if (++score[1 - i] >= WinPoint) IsRunning = false;
+					else Init(i == 0);
+				}
+				else
+				{
+					discPos.Y = -discPos.Y + 2 * (i == 0 ? Size : (1 - Size));
+					dir.Y = -dir.Y;
+//					if (x < Window1.barRate * 0.3) dir.Y -= 0.016;
+//					else if (x > Window1.barRate * 0.7) dir.Y += 0.016;
+				}
+			}
+		}
+		public void Move(bool bFirst, bool bLeft)
+		{
+			int i = bFirst ? 0 : 1;
+			barPos[i] += (bLeft ? -1 : 1) * Size / 3;
+			if (barPos[i] < 0)
+				barPos[i] = 0;
+			else if (barPos[i] > 1 - Window1.barRate)
+				barPos[i] = 1 - Window1.barRate;
+		}
+	}
+}
---
+++ TennisGame/Window1.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TennisGame
+{
+	/// <summary>
+	/// Window1.xaml の相互作用ロジック
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		private const int portno = 8102;
+		private const int mgn = 50;
+		internal const float barRate = 0.2F;
+		internal const float boardRate = 0.3F;
+		private Server svr;
+		private bool bFirst = false;
+		private DispatcherTimer timer1 = new DispatcherTimer();
+		public Window1()
+		{
+			InitializeComponent();
+			gr.SizeChanged += new SizeChangedEventHandler(gr_SizeChanged);
+			SetDiskPos(new Point(100, 150));
+			SetPadPos(true, 11);
+		}
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				if (Properties.Settings.Default.Server == Environment.MachineName
+					|| Properties.Settings.Default.Server == "localhost")
+				{
+					bFirst = true;
+					System.Runtime.Remoting.Channels.ChannelServices.RegisterChannel(
+								new System.Runtime.Remoting.Channels.Tcp.TcpChannel(portno), false);
+					BackgroundWorker bw = new BackgroundWorker();
+					bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+					bw.RunWorkerAsync();
+					System.Threading.Thread.Sleep(100);
+				}
+			}
+			catch { bFirst = false; }
+			svr = (Server)Activator.GetObject(typeof(Server), "tcp://" +
+				Properties.Settings.Default.Server + ":" + portno + "/TennisGame");
+			if (!bFirst)
+			{
+				ca.Position = new Point3D(-ca.Position.X, ca.Position.Y, -ca.Position.Z);
+				ca.LookDirection = new Vector3D(-ca.LookDirection.X, ca.LookDirection.Y, -ca.LookDirection.Z);
+				svr.IsWaiting = false;
+			}
+			timer1.Interval = TimeSpan.FromMilliseconds(50);
+			timer1.Tick += new EventHandler(timer1_Tick);
+			timer1.Start();
+			MouseMove += new MouseEventHandler(Window1_MouseMove);
+		}
+		void Window1_MouseMove(object sender, MouseEventArgs e)
+		{
+			Point p = e.GetPosition(Viewport3D_test);
+			VisualTreeHelper.HitTest(Viewport3D_test, null, HitTest, new PointHitTestParameters(p));
+		}
+		HitTestResultBehavior HitTest(HitTestResult ht)
+		{
+			var r = ht as RayMeshGeometry3DHitTestResult;
+			if (r == null) return HitTestResultBehavior.Continue;
+			svr.Move(bFirst, r.PointHit.X < (svr.BarPos(bFirst) - 0.5 + barRate / 2) * 300);
+			return HitTestResultBehavior.Stop;
+		}
+		void timer1_Tick(object sender, EventArgs e)
+		{
+			timer1.Stop();
+			try
+			{
+				bool bRun = svr.IsRunning;
+				button1.Visibility = bRun ? Visibility.Collapsed : Visibility.Visible;
+				if (bRun && bFirst) svr.Do();
+				SetDiskPos(svr.DiscPos);
+				SetPadPos(bFirst, svr.BarPos(bFirst));
+				SetPadPos(!bFirst, svr.BarPos(!bFirst));
+				label1.Content = string.Format("({0}-{1})",
+					svr.Score(bFirst), svr.Score(!bFirst));
+				label2.Visibility = Visibility.Collapsed; 
+				if (!svr.IsRunning)
+				{
+					label2.Visibility = Visibility.Visible; 
+					label2.Content = svr.Score(bFirst) == Server.WinPoint ? "You win !" : "You loss";
+				}
+				timer1.Start();
+			}
+			catch { }
+		}
+		void bw_DoWork(object sender, DoWorkEventArgs e)
+		{
+			System.Runtime.Remoting.RemotingConfiguration.RegisterWellKnownServiceType(
+				typeof(Server), "TennisGame", System.Runtime.Remoting.WellKnownObjectMode.Singleton);
+		}
+		private void SetDiskPos(Point pos)
+		{
+			var tt = (Transform3DGroup)Model_test_disk.Transform;
+			var tr = (TranslateTransform3D)tt.Children[0];
+			tr.OffsetX = (pos.X - 0.5) * 300;
+			tr.OffsetZ = (pos.Y - 0.5) * 400;
+		}
+		private void SetPadPos(bool bOne, double x)
+		{
+			var tt = (Transform3DGroup)(bOne ? Model_test_pad1 : Model_test_pad2).Transform;
+			var tr = (TranslateTransform3D)tt.Children[0];
+			tr.OffsetX = (x - 0.5 + barRate / 2) * 300;
+		}
+		void gr_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			Viewport3D_test.Width = gr.ActualWidth;
+			Viewport3D_test.Height = gr.ActualHeight;
+		}
+		private void button1_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				svr.Reset();
+				button1.Visibility = Visibility.Collapsed;
+			}
+			catch { }
+		}
+	}
+}
